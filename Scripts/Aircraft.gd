8478c8f825242ca34e632d544b2c343c5883282d
Harry Brown <rhodiumthedev@gmail.com>
2021-04-23 00:08:13 +0100

    Final and now fixed physics implementation, and further tweaking

--- Scripts/Aircraft.gd
+++ Scripts/Aircraft.gd
@@ -18,26 +18,26 @@ func _physics_process(delta):
 	var velocity = Vector3(0,0,0).distance_to(linear_velocity)
 
 	for lifting_body in lifting_bodies:
-		add_force(lifting_body.calculate_lift(delta, velocity, linear_velocity) * global_transform.basis.y, lifting_body.translation)
+		add_force(lifting_body.calculate_lift(delta, velocity, linear_velocity).rotated((Vector3(0,1,0).cross(global_transform.basis.y)).normalized(), Vector3(0,1,0).angle_to(global_transform.basis.y)), lifting_body.translation)
 		pass
 	
 	if Input.is_action_pressed("pitch_down"):
-		add_force(global_transform.basis.y*150, global_transform.basis.z*5)
+		add_force(global_transform.basis.y*300, global_transform.basis.z*5)
 	
 	if Input.is_action_pressed("pitch_up"):
-		add_force(global_transform.basis.y*-150, global_transform.basis.z*5)
+		add_force(global_transform.basis.y*-300, global_transform.basis.z*5)
 	
 	if Input.is_action_pressed("roll_CW"):
-		add_force(global_transform.basis.y*150, global_transform.basis.x * -3.246)
+		add_force(global_transform.basis.y*300, global_transform.basis.x * -3.246)
 	
 	if Input.is_action_pressed("roll_ACW"):
-		add_force(global_transform.basis.y*150, global_transform.basis.x * 3.246)
+		add_force(global_transform.basis.y*300, global_transform.basis.x * 3.246)
 	
 	if Input.is_action_pressed("yaw_left"):
-		add_force(global_transform.basis.x*150, global_transform.basis.z*5)
+		add_force(global_transform.basis.x*300, global_transform.basis.z*5)
 	
 	if Input.is_action_pressed("yaw_right"):
-		add_force(global_transform.basis.x*-150, global_transform.basis.z*5)
+		add_force(global_transform.basis.x*-300, global_transform.basis.z*5)
 	
 	
 	
